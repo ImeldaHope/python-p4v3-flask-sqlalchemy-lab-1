d934e21e7c8c753096497c37caebaa5c07caaae9
Imelda Hope <imeldalarusso@gmail.com>
2024-10-01 13:20:01 +0300

    creates a flask-sqlalchemy model-view

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +21,37 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def earthquakes(id):
+    earthquake = db.session.get(Earthquake, id)
 
+    if earthquake:        
+        return jsonify({
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }), 200
+    else:        
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def magnitude(magnitude):
+    earthquakes = db.session.query(Earthquake).filter(Earthquake.magnitude >= magnitude).all()
+
+    response_data = {
+        "count": len(earthquakes),
+        "quakes": [
+            {
+                "id": quake.id,
+                "location": quake.location,
+                "magnitude": quake.magnitude,
+                "year": quake.year
+            } for quake in earthquakes
+        ]
+    }
+    
+    return jsonify(response_data), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, Column, String, Integer, Float
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
@@ -7,3 +7,19 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "eathquakes"
+
+    id = Column(Integer(), primary_key = True)
+    magnitude = Column(Float())
+    location = Column(String())
+    year = Column(Integer())
+
+    def __init__(self, magnitude=None, location=None, year=None, id=None):
+        self.id = id  
+        self.magnitude = magnitude
+        self.location = location
+        self.year = year
+
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
